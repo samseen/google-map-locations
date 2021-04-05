ea9b7e0bd41e5e9fad1d195510efc0c92b559ef5
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-05 21:20:50 +0100

    Project completion.

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-function initMap() {
+function initMap(){
 
-    // Map Option
+    // Map option
+
     var options = {
-        center: {lat: 8.8764, lng: 7.2437},
+        center: {lat: 8.8764, lng:7.2437},
         zoom: 10
     }
 
     //New Map
-    map = new google.maps.Map(document.getElementById('map'), options);
+    map = new google.maps.Map(document.getElementById("map"),options)
 
-    //Marker
-    /*const marker = new google.maps.Marker({
-        position:{lat: 9.0040, lng: 7.2691},
-        map: map,
-        icon: "https://img.icons8.com/nolan/2x/marker.png"
-    });
+    //listen for click on map location
 
-    //InfoWindow
+    google.maps.event.addListener(map, "click", (event) => {
+        //add Marker
+        addMarker({location:event.latLng});
+    })
 
-    const detailWindow = new google.maps.InfoWindow({
-        content: `<h2>Our Aiport</h2>`
-    });
+    //Add Markers to Array
 
-    marker.addListener("mouseover", () => {
-        detailWindow.open(map, marker);
-    })*/
+    let MarkerArray = [
+        {location:{lat: 9.0040, lng: 7.2691},
+            imageIcon: "https://img.icons8.com/nolan/2x/marker.png",
+            content: `<h2>Abuja City</h2>`
+        },
+        {location:{lat: 8.9914, lng: 7.3948}},
+        {location:{lat: 9.0602, lng: 7.4898},
+            content: `<h2>National Mosque</h2>`
+        }
+    ]
 
-    // Add Marker
-    function addMarker(location) {
-        const marker = new google.maps.Marker({
-            position:location,
-            map: map,
-            icon: "https://img.icons8.com/nolan/2x/marker.png"
-        });
+    // loop through marker
+    for (let i = 0; i < MarkerArray.length; i++){
+        addMarker(MarkerArray[i]);
     }
 
-    addMarker({lat: 9.0040, lng: 7.2691});
-    addMarker({lat: 8.9914, lng: 7.3948});
-    addMarker({lat: 9.0602, lng: 7.4898});
+    // Add Marker
+
+    function addMarker(property){
+
+        const marker = new google.maps.Marker({
+            position:property.location,
+            map:map,
+            //icon: property.imageIcon
+        });
+
+        // Check for custom Icon
+
+        if(property.imageIcon){
+            // set image icon
+            marker.setIcon(property.imageIcon)
+        }
+
+        if(property.content){
+            const detailWindow = new google.maps.InfoWindow({
+                content: property.content
+            });
+
+            marker.addListener("mouseover", () =>{
+                detailWindow.open(map, marker);
+            })
+        }
+    }
 }
